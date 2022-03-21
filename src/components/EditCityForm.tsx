0b18dbf9f63ec641c934abe513b013b740c511a6
Yue Wu <wuyueashley@outlook.com>
2022-03-21 17:47:53 -0400

    put team selection into multiple columns so modal buttons don't get cut off

--- src/components/EditCityForm.tsx
+++ src/components/EditCityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonButton, IonCol,IonInput, IonItem, IonLabel, IonList, IonRadio, IonRadioGroup, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonInput, IonItem, IonLabel, IonRadio, IonRadioGroup, IonRow, IonTitle } from '@ionic/react';
 import City from './City';
 import TEAM from './Team';
 
@@ -15,76 +15,83 @@ const EditCityForm: React.FC<EditCityFormProps> = ({ city, citySetter, isModalOp
     const [soldiers, setSoldiers] = useState<number>(city.getNumSoldiers());
   
     return (
-        <form className="ion-padding">
-            <IonToolbar>
-                <IonTitle>{"Editing " + city.name}</IonTitle>
-            </IonToolbar>
-            <IonRow>
-                <IonCol>Select Team</IonCol>
-                <IonCol>
-                    <IonList>
+        // IonContent to enable scrolling if browser size cuts off the buttons
+        <IonContent> 
+            <form className="ion-padding">
+                <IonItem>
+                    <IonTitle>{"Editing " + city.name}</IonTitle>
+                </IonItem>
+                <IonItem>
+                    <IonCol>
+                        <IonLabel>Select Team</IonLabel>
+                    </IonCol>
+                    <IonCol>
                         <IonRadioGroup value={team} placeholder="shadow" onIonChange={(e) => setTeam(e.detail.value)}>
+                            <IonRow>
                             {
                                 Object.keys(TEAM).map(val => {
                                     val = val.toLowerCase();
-                                    return <IonItem>
-                                        <IonLabel> {val}</IonLabel>
-                                        <IonRadio value={val}></IonRadio>
-                                    </IonItem>
+                                    return <IonCol>
+                                        <IonItem>
+                                            <IonLabel> {val}</IonLabel>
+                                            <IonRadio value={val}></IonRadio>
+                                        </IonItem>
+                                    </IonCol>
                                 })
                             }
+                            </IonRow>
                         </IonRadioGroup>
-                    </IonList>
-                </IonCol>
-            </IonRow>
-            
-            <IonItem>
-                <IonLabel position="floating">Number of Soldiers</IonLabel>
-                <IonInput value={soldiers} onIonChange={(e) => {
-                    var newValue = e.detail.value;
-                    if (newValue) {
-                        setSoldiers(parseInt(newValue));
-                    }
-                } } onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        e.preventDefault();
-                        document.getElementById("saveButton")?.click();
-                    }
-                } }/>
-            </IonItem>
+                    </IonCol>
+                </IonItem>
+                
+                <IonItem>
+                    <IonLabel position="floating">Number of Soldiers</IonLabel>
+                    <IonInput value={soldiers} onIonChange={(e) => {
+                        var newValue = e.detail.value;
+                        if (newValue) {
+                            setSoldiers(parseInt(newValue));
+                        }
+                    } } onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            e.preventDefault();
+                            document.getElementById("saveButton")?.click();
+                        }
+                    } }/>
+                </IonItem>
 
-            <IonRow>
-                <IonCol>
-                    <IonButton className="ion-margin-bottom" type="button" expand="block" onClick={() => {
-                            setTeam(city.getDefaultTeam())
-                            setSoldiers(city.getDefaultSoldiers())
+                <IonRow>
+                    <IonCol>
+                        <IonButton className="ion-margin" type="button" expand="block" onClick={() => {
+                                setTeam(city.getDefaultTeam())
+                                setSoldiers(city.getDefaultSoldiers())
+                            }}>
+                                Reset
+                        </IonButton>
+                    </IonCol>
+                    <IonCol>
+                        <IonButton id="saveButton" className="ion-margin" type="button" expand="block" onClick={() => {
+                            city.team = team;
+                            city.soldiers = soldiers;
+                            citySetter(city);
+                            isModalOpenSetter(false); // close modal!
+
+                            // persist to local storage 
+                            // await Storage.set({
+                            //     key: String(city.num),
+                            //     value: city.toJson(),
+                            // })
+                            window.localStorage.setItem(String(city.num), city.toJson());
+
+                            //alert((team ?? "none") + " with soldiers: " + soldiers); // DEBUG ONLY
+                            
                         }}>
-                            Reset
-                    </IonButton>
-                </IonCol>
-                <IonCol>
-                    <IonButton id="saveButton" className="ion-margin-bottom" type="button" expand="block" onClick={() => {
-                        city.team = team;
-                        city.soldiers = soldiers;
-                        citySetter(city);
-                        isModalOpenSetter(false); // close modal!
-
-                        // persist to local storage 
-                        // await Storage.set({
-                        //     key: String(city.num),
-                        //     value: city.toJson(),
-                        // })
-                        window.localStorage.setItem(String(city.num), city.toJson());
-
-                        //alert((team ?? "none") + " with soldiers: " + soldiers); // DEBUG ONLY
-                        
-                    }}>
-                        Save
-                    </IonButton>
-                </IonCol>
-            </IonRow>
-            
-        </form>
+                            Save
+                        </IonButton>
+                    </IonCol>
+                </IonRow>
+                
+            </form>
+        </IonContent>
   );
 };
 
